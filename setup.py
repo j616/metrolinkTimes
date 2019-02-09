28f0cd1e683fe896248e8786fd301da3a1201142
James Sandford <jamesmsandford@googlemail.com>
2019-02-09 22:10:29 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if not Path(logPath).exists():
     os.chown(logPath, getpwnam('mltimes').pw_uid, getpwnam('mltimes').pw_gid)
 
 setup(name='metrolinkTimes',
-      version='1.2',
+      version='1.3',
       description=('Track trams on the metrolink network & estimate their due '
                    'times'),
       author='James Sandford',
